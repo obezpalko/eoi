7f7f657c3968427579e75e342d6d89da70bc09ec
Alex Bezpalko <alexb@comet.com>
2026-01-21 08:06:45 +0100

    feat: copy files from `quartz/root` to the output directory.

--- quartz/plugins/emitters/static.ts
+++ quartz/plugins/emitters/static.ts
@@ -7,6 +7,7 @@ import { dirname } from "path"
 export const Static: QuartzEmitterPlugin = () => ({
   name: "Static",
   async *emit({ argv, cfg }) {
+    // Copy files from quartz/static to output/static
     const staticPath = joinSegments(QUARTZ, "static")
     const fps = await glob("**", staticPath, cfg.configuration.ignorePatterns)
     const outputStaticPath = joinSegments(argv.output, "static")
@@ -18,6 +19,19 @@ export const Static: QuartzEmitterPlugin = () => ({
       await fs.promises.copyFile(src, dest)
       yield dest
     }
+
+    // Copy files from quartz/root to output/
+    const rootPath = joinSegments(QUARTZ, "root")
+    if (fs.existsSync(rootPath)) {
+      const r_fps = await glob("**", rootPath, cfg.configuration.ignorePatterns)
+      for (const fp of r_fps) {
+        const src = joinSegments(rootPath, fp) as FilePath
+        const dest = joinSegments(argv.output, fp) as FilePath
+        await fs.promises.mkdir(dirname(dest), { recursive: true })
+        await fs.promises.copyFile(src, dest)
+        yield dest
+      }
+    }
   },
-  async *partialEmit() {},
+  async *partialEmit() { },
 })
